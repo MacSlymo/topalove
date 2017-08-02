b8de6d6488ee3a3ddaaed0cb91057bd1f9ade8d1
Mathieu Revillion <mathieu.revillion@gmail.com>
2017-08-02 17:23:06 +0200

    start the 80s datas and the algorithm

--- routes/versus.js
+++ routes/versus.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const ensureLoggedIn = require("connect-ensure-login").ensureLoggedIn;
+const mongoose = require("mongoose");
 
 /* GET versus page */
-
 
 router.get("/versus", ensureLoggedIn(), (req, res, next) => {
   res.render("versus", {
@@ -12,3 +12,18 @@ router.get("/versus", ensureLoggedIn(), (req, res, next) => {
 });
 
 module.exports = router;
+
+/*
+router.post("/versus", (req, res, next) => {
+  let userId = req.session.currentUser._id;
+  let movie1Id = req.body.movie1.id;
+  let movie2Id = req.body.movie2.id;
+  let winningMovieId = ;
+
+  res.send(``);
+
+  res.redirect("versus", {
+    styles: ["versus.css"]
+  });
+});
+*/
