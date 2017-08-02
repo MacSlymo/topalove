d90e9c91aa607632e7b9cbceea89483d7a7c6bc2
FrancoisKIEFFER <frankie.prime@gmail.com>
2017-08-02 10:55:20 +0200

    data et fix de versus

---
+++ bin/data/quatreVingtDix.js
@@ -0,0 +1,497 @@
+{
+  title: "The Blade",
+  category: "Action",
+  cover:
+    "http://2.bp.blogspot.com/-Rq7pUbr2Ke8/TV0ZysGsP8I/AAAAAAAAAKw/OJC1-sTfu2w/s1600/IMG_0019.JPG",
+  decade: 90
+},
+{
+  title: "Pulp Fiction",
+  category: "Action",
+  cover:
+    "https://www.retourverslecinema.com/wp-content/uploads/2013/09/Pulp-fiction-1994.jpg",
+  decade: 90
+},
+{
+  title: "Fargo",
+  category: "Drama",
+  cover:
+    "https://media.senscritique.com/media/000013492686/source_big/Fargo.jpg",
+  decade: 90
+},
+{
+  title: "Truman Show",
+  category: "Drama",
+  cover:
+    "https://media.senscritique.com/media/000007087731/source_big/The_Truman_Show.jpg",
+  decade: 90
+},
+{
+  title: "The Iron Giant",
+  category: "Animation",
+  cover:
+    "https://s-media-cache-ak0.pinimg.com/originals/6d/a3/ca/6da3cae67284f2a7e1ded45550ee8a04.jpg",
+  decade: 90
+},
+{
+  title: "Casino",
+  category: "Drama",
+  cover:
+    "https://www.critikat.com/wp-content/uploads/2008/09/artoff2343.jpg",
+  decade: 90
+},
+{
+  title: "The Silence of the lambs",
+  category: "Drama",
+  cover:
+    "https://fanart.tv/fanart/movies/274/movieposter/the-silence-of-the-lambs-5487909ca88be.jpg",
+  decade: 90
+},
+{
+  title: "The Sixth Sense",
+  category: "Horror",
+  cover:
+    "https://fanart.tv/fanart/movies/745/movieposter/the-sixth-sense-522bc63936280.jpg",
+  decade: 90
+},
+{
+  title: "Porco Rosso",
+  category: "Animation",
+  cover:
+    "https://2.bp.blogspot.com/-FQB3319-_ZM/UCEz_vbxWBI/AAAAAAAAK4M/bKEHPIByFB8/s1600/Porco+Rosso+%281992%29+1.jpg",
+  decade: 90
+},
+{
+  title: "Heat",
+  category: "Action",
+  cover:
+    "http://www.dvdclassik.com/upload/images/affiches/heat1.jpg",
+  decade: 90
+},
+{
+  title: "Carlito's Way",
+  category: "Drama",
+  cover:
+    "https://s-media-cache-ak0.pinimg.com/736x/8d/a3/33/8da333d8235835b19a1ac4d0a532b501--carlito-brigante-carlitos-way.jpg",
+  decade: 90
+},
+{
+  title: "L.A. Confidential",
+  category: "Drama",
+  cover:
+    "http://www.impawards.com/1997/posters/la_confidential_ver2_xlg.jpg",
+  decade: 90
+},
+{
+  title: "Reservoir Dogs",
+  category: "Action",
+  cover:
+    "https://ae01.alicdn.com/kf/HTB1uwKTIpXXXXa7XVXXq6xXFXXX6/Free-shipping-font-b-Reservoir-b-font-font-b-Dogs-b-font-font-b-Poster-b.jpg",
+  decade: 90
+},
+{
+  title: "Saving Private Ryan",
+  category: "Action",
+  cover:
+    "http://tr.web.img3.acsta.net/pictures/bzp/01/18598.jpg",
+  decade: 90
+},
+{
+  title: "Princess Mononoke",
+  category: "Animation",
+  cover:
+    "http://img12.deviantart.net/ee3c/i/2016/093/e/8/princess_mononoke__alternative_movie_poster_by_marioredsigns-d9xm2lk.jpg",
+  decade: 90
+},
+{
+  title: "Last Action Hero",
+  category: "Action",
+  cover:
+    "http://files.doobybrain.com/wp-content/uploads/2015/01/last-action-hero-poster-930x1340.jpg",
+  decade: 90
+},
+{
+  title: "Ghost Dog",
+  category: "Action",
+  cover:
+    "http://www.impawards.com/2000/posters/ghost_dog_the_way_of_the_samurai_ver3_xlg.jpg",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
+{
+  title: "",
+  category: "",
+  cover:
+    "",
+  decade: 90
+},
